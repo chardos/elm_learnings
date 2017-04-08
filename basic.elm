8733baf44c92313a107b7b696314cea3d4d8a562
Richard Tan <richard.tan@crownbet.com.au>
2017-04-09 09:21:46 +1000

    Politely

--- basic.elm
+++ basic.elm
@@ -2,7 +2,14 @@ module Main exposing (..)
 
 import Html exposing (text)
 
+politely: String -> String
+politely phrase =
+    "Excuse me, " ++ phrase
+
+ask : String -> String -> String
 ask thing place = "Is there a " ++ thing ++ " in the " ++ place ++ "?"
 
 main =
-    text <| ask "rabbit" "garden"
+    text
+    <| politely
+    <| ask "rabbit" "garden"
