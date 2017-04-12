4c5e044d2ef5c03cdb0a884d6585c18549579b93
Richard Tan <richard.tan@crownbet.com.au>
2017-04-12 20:41:21 +1000

    Face generator

---
+++ html/facegenerator.elm
@@ -0,0 +1,37 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html exposing (beginnerProgram)
+import Html.Events exposing (..)
+
+-- Four parts
+
+model =
+    { showFace = False }
+
+
+type Msg =
+    ShowFace
+
+update msg model_ =
+    case msg of
+        ShowFace -> { model_ | showFace = True }
+
+
+view model_ =
+    div []
+        [
+            h1 [] [text "Face generator"],
+            button [onClick ShowFace] [text "Face me"],
+            if model_.showFace then
+                text "Face!"
+            else
+                text ""
+        ]
+
+main =
+    beginnerProgram {
+        model = model,
+        update = update,
+        view = view
+    }
